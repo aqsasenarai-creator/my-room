e7792d846a6e90229f31e9f4454e5828be08cd43
aqsasenarai-creator <aqsasenarai@gmail.com>
2025-09-30 16:39:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Menu Hamburguer untuk tampilan mobile
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
 
